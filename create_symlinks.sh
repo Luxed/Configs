e3e7f9955e0f8d39af01a2a357bd1c6160ef447a
Luxed <devildead13@gmail.com>
2020-04-26 20:56:42 -0400

    Add symlinks creation script

---
+++ create_symlinks.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+dir=$(pwd)
+
+ln -s $dir/home/.config/alacritty ~/.config/alacritty
+ln -s $dir/home/.config/i3 ~/.config/i3
+ln -s $dir/home/.config/rofi ~/.config/rofi
+
+ln -s $dir/home/.zshrc
