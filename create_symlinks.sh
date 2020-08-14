a8663affb024a58c5fff6d2dfbfd1c5a4e144ae9
Luxed <devildead13@gmail.com>
2020-08-14 13:47:36 -0400

    Add polybar config to symlink script

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -3,8 +3,9 @@
 dir=$(pwd)
 
 ln -s $dir/home/.config/alacritty ~/.config/alacritty
-ln -s $dir/home/.config/i3 ~/.config/i3
-ln -s $dir/home/.config/rofi ~/.config/rofi
 ln -s $dir/home/.config/dunst ~/.config/dunst
+ln -s $dir/home/.config/i3 ~/.config/i3
+ln -s $dir/home/.config/polybar ~/.config/polybar
+ln -s $dir/home/.config/rofi ~/.config/rofi
 
 ln -fs $dir/home/.zshrc ~/.zshrc
