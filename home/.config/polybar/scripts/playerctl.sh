b37d243990404ec03dd2ac0177c130de879abb81
Luxed <devildead13@gmail.com>
2020-08-26 09:25:18 -0400

    Replace player-cmus script by much simpler playerctl script

---
+++ home/.config/polybar/scripts/playerctl.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+player_status=$(playerctl status 2> /dev/null)
+player_metadata_formated=$(playerctl metadata --format '{{artist}} - {{title}} | {{duration(position)}}/{{duration(mpris:length)}}')
+
+if [ "$player_status" = "Playing" ]; then
+    echo " $player_metadata_formated"
+elif [ "$player_status" = "Paused" ]; then
+    echo " $player_metadata_formated"
+else
+    echo ""
+fi
