03abb096d4788231da84df139dfa6b96bfee0dfc
Luxed <devildead13@gmail.com>
2020-08-26 09:24:13 -0400

    Add mpd configurations

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -5,6 +5,8 @@ dir=$(pwd)
 ln -s $dir/home/.config/alacritty ~/.config/alacritty
 ln -s $dir/home/.config/dunst ~/.config/dunst
 ln -s $dir/home/.config/i3 ~/.config/i3
+ln -s $dir/home/.config/mpd ~/.config/mpd
+ln -s $dir/home/.config/mpDris2 ~/.config/mpDris2
 ln -s $dir/home/.config/polybar ~/.config/polybar
 ln -s $dir/home/.config/rofi ~/.config/rofi
 
