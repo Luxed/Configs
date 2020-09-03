b6a5a3bdcf19428636726a1efdd075c3e8d5f124
Luxed <devildead13@gmail.com>
2020-09-02 21:24:05 -0400

    Fix polybar + make script behave differently depending on the monitor

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -6,7 +6,20 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload primary &
-done
+primary_monitors=$(polybar --list-monitors | grep primary | cut -d":" -f1)
+other_monitors=$(polybar --list-monitors | grep -v primary | cut -d":" -f1)
+
+if [ -z $primary_monitors ]
+then
+    for m in $(polybar --list-monitors | cut -d":" -f1); do
+        MONITOR=$m polybar --reload primary &
+    done
+else
+    for m in $primary_monitors; do
+        MONITOR=$m polybar --reload primary &
+    done
+
+    for m in $other_monitors; do
+        MONITOR=$m polybar --reload other &
+    done
+fi
