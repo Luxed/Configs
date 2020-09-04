ba7bfd2d637a2fd298e835c005ea5a2312aac524
Luxed <devildead13@gmail.com>
2020-09-04 09:11:31 -0400

    Fix playerctl script for chromium base browsers (because they don't expose "mpris:length")

--- home/.config/polybar/scripts/playerctl_progress.py
+++ home/.config/polybar/scripts/playerctl_progress.py
@@ -16,16 +16,12 @@ status_chars = {
 status = os.popen("playerctl status 2> /dev/null").read().rstrip()
 current_metadata = os.popen("playerctl metadata --format '{{artist}} - {{title}}' 2> /dev/null").read().rstrip()
 
-if status == "Playing" or status == "Paused":
-    position = float(os.popen("playerctl metadata --format \"{{position}}\"").read().strip())
-    length = float(os.popen("playerctl metadata --format \"{{mpris:length}}\"").read().strip())
-    max_percent = 100
-    current = (position / length) * max_percent
-
+def format_progress_bar(progress_chars, progress_size, current):
+    current_percent = current * 100
     progress_steps = len(progress_chars)
-    progress_ratio = (float(max_percent) / float(progress_steps)) / float(progress_size)
+    progress_ratio = (float(100) / float(progress_steps)) / float(progress_size)
 
-    current_progress = (current / progress_ratio)
+    current_progress = (current_percent / progress_ratio)
 
     progress_finished = int(current_progress/progress_steps)
 
@@ -35,7 +31,20 @@ if status == "Playing" or status == "Paused":
     before = progress_chars[progress_steps-1]*progress_finished
     after = " "*(progress_size - progress_finished - 1)
 
-    print("{} {} ▕{}{}{}▏".format(status_chars[status], current_metadata, before, char, after))
+    return "▕{}{}{}▏".format(before, char, after)
+
+if status == "Playing" or status == "Paused":
+    current_metadata_status = "{} {}".format(status_chars[status], current_metadata)
+
+    try:
+        position = float(os.popen("playerctl metadata --format \"{{position}}\"").read().strip())
+        length = float(os.popen("playerctl metadata --format \"{{mpris:length}}\"").read().strip())
+        current = (position / length)
+        progress_bar = format_progress_bar(progress_chars, progress_size, current)
+
+        print("{} {}".format(current_metadata_status, progress_bar))
+    except ValueError:
+        print(current_metadata_status)
 elif status == "Stopped":
     print("")
 else:
