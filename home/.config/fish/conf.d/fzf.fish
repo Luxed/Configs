9b42d2a61d9c69aceb3bf858bd56d4f2d71ed92d
Luxed <devildead13@gmail.com>
2021-10-25 23:10:01 -0400

    Add config for nice fzf driven cd

---
+++ home/.config/fish/conf.d/fzf.fish
@@ -0,0 +1,93 @@
+# From https://github.com/junegunn/fzf/blob/edac9820b54d8db7ef667c2d79b9e526c625f59d/shell/key-bindings.fish#L75
+
+function fcd
+    fzf-cd-widget $argv
+end
+
+function fzf-cd-widget -d "Change directory"
+    set -l commandline (__fzf_parse_commandline)
+    set -l dir $commandline[1]
+    set -l fzf_query $commandline[2]
+    set -l prefix $commandline[3]
+
+    test -n "$FZF_ALT_C_COMMAND"; or set -l FZF_ALT_C_COMMAND "
+    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
+    -o -type d -print 2> /dev/null | sed 's@^\./@@'"
+    test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+    begin
+        set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS"
+        eval "$FZF_ALT_C_COMMAND | "(__fzfcmd)' +m --query "'$fzf_query'"' | read -l result
+
+        if [ -n "$result" ]
+            cd $result
+
+            # Remove last token from commandline.
+            commandline -t ""
+            commandline -it -- $prefix
+        end
+    end
+
+    commandline -f repaint
+end
+
+function __fzfcmd
+    test -n "$FZF_TMUX"; or set FZF_TMUX 0
+    test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
+    if [ -n "$FZF_TMUX_OPTS" ]
+        echo "fzf-tmux $FZF_TMUX_OPTS -- "
+    else if [ $FZF_TMUX -eq 1 ]
+        echo "fzf-tmux -d$FZF_TMUX_HEIGHT -- "
+    else
+        echo "fzf"
+    end
+end
+
+function __fzf_parse_commandline -d 'Parse the current command line token and return split of existing filepath, fzf query, and optional -option= prefix'
+    set -l commandline (commandline -t)
+
+    # strip -option= from token if present
+    set -l prefix (string match -r -- '^-[^\s=]+=' $commandline)
+    set commandline (string replace -- "$prefix" '' $commandline)
+
+    # eval is used to do shell expansion on paths
+    eval set commandline $commandline
+
+    if [ -z $commandline ]
+        # Default to current directory with no --query
+        set dir '.'
+        set fzf_query ''
+    else
+        set dir (__fzf_get_dir $commandline)
+
+        if [ "$dir" = "." -a (string sub -l 1 -- $commandline) != '.' ]
+            # if $dir is "." but commandline is not a relative path, this means no file path found
+            set fzf_query $commandline
+        else
+            # Also remove trailing slash after dir, to "split" input properly
+            set fzf_query (string replace -r "^$dir/?" -- '' "$commandline")
+        end
+    end
+
+    echo $dir
+    echo $fzf_query
+    echo $prefix
+end
+
+
+function __fzf_get_dir -d 'Find the longest existing filepath from input string'
+    set dir $argv
+
+    # Strip all trailing slashes. Ignore if $dir is root dir (/)
+    if [ (string length -- $dir) -gt 1 ]
+        set dir (string replace -r '/*$' -- '' $dir)
+    end
+
+    # Iteratively check if dir exists and strip tail end of path
+    while [ ! -d "$dir" ]
+        # If path is absolute, this can keep going until ends up at /
+        # If path is relative, this can keep going until entire input is consumed, dirname returns "."
+        set dir (dirname -- "$dir")
+    end
+
+    echo $dir
+end
