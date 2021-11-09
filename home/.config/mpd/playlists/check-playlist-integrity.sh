5d1e514a357f80db90165f43abb422ec8cbe4eb6
Luxed <devildead13@gmail.com>
2021-11-09 10:57:25 -0500

    Add music to playlist + improve integrity script

--- home/.config/mpd/playlists/check-playlist-integrity.sh
+++ home/.config/mpd/playlists/check-playlist-integrity.sh
@@ -5,9 +5,19 @@ set -e
 MUSIC_DIR="/mnt/Data-Server/Corentin/Music"
 PLAYLIST_FILE="./everyday.m3u"
 
+MUSIC_TOTAL=0
+MUSIC_ISSUES=0
+
+printf "Starting music files integrity check\n"
+
 while IFS= read -r line; do
-    #printf '%s\n' "$MUSIC_DIR/$line"
-    if [ ! -f "$MUSIC_DIR/$line" ]; then
-        printf '%s: does not exist!\n' "$MUSIC_DIR/$line"
+    MUSIC_TOTAL=$(( MUSIC_TOTAL + 1 ))
+    FILE_PATH="$MUSIC_DIR/$line"
+    #printf '%s\n' "$FILE_PATH"
+    if [ ! -f "$FILE_PATH" ]; then
+        MUSIC_ISSUES=$(( MUSIC_ISSUES + 1 ))
+        printf "%s: does not exist!\n" "$FILE_PATH"
     fi
 done < $PLAYLIST_FILE
+
+printf "%d/%d music files with issues.\n" $MUSIC_ISSUES $MUSIC_TOTAL
