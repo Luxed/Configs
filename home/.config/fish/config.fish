fcac58462d0c1f60efbf594d620d4a3a7161a5cf
Luxed <devildead13@gmail.com>
2023-03-09 20:32:11 -0500

    Update config

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -9,3 +9,6 @@ set -px --path PATH "/home/cbrunel/.bun/bin"
 # Bobthefish theme configuration
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/cbrunel/work/tmp/google-cloud-sdk/path.fish.inc' ]; . '/home/cbrunel/work/tmp/google-cloud-sdk/path.fish.inc'; end
